2aef81ab0c9700d728d4fa630e33acb01dcb61f5
ashikbhuyan17 <ashikbhuyan.swe.diu@gmail.com>
2023-03-14 17:33:40 +0600

    Working On Layout Page

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+// third party
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+
+// project imports
+
 import App from './App';
+import { store } from './store';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>
 );
